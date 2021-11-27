9eb19676eac239d47d76ee37165f9bb5231da771
Saumyadip Packray <saumyadip.packray@ext.zestmoney.in>
2021-11-27 20:04:47 +0530

    main

---
+++ src/main/java/com/businessManager/BusinessManagerApplication.java
@@ -0,0 +1,13 @@
+package com.businessManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/businessManager/BusinessManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.businessManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
